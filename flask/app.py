f4f4c338eca38b56d01b5cb9be43f3b69189f326
michodgs24 <michodgs@gmail.com>
2020-08-19 10:17:46 +0000

    Add create.html to templates& create database connection

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'third_milestone_project'
+app.config['MONGO_URI'] = 'mongodb+srv://root:r00tUser@mongo-mini-pro.dhhij.mongodb.net/third_milestone_project?retryWrites=true&w=majority'
+
+mongo = pyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_activity')
+def get_tasks():
+    return render_template("create.html", activity=mongo.db.activity.find())
+
 
 
 if __name__ == '__main__':
