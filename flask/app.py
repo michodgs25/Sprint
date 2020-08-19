e1ea1cc09ecc206e0a9d8a1736c7d039b3e5c533
michodgs24 <michodgs@gmail.com>
2020-08-19 10:28:45 +0000

    Update create page& app.py database connection

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,16 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from bson.objectid import ObjectId 
+
+
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'third_milestone_project'
 app.config['MONGO_URI'] = 'mongodb+srv://root:r00tUser@mongo-mini-pro.dhhij.mongodb.net/third_milestone_project?retryWrites=true&w=majority'
 
-mongo = pyMongo(app)
+mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/get_activity')
