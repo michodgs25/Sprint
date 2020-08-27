c7f7447629819bbc3cd81da56ffe387eff9a7155
michodgs24 <michodgs@gmail.com>
2020-08-27 13:00:47 +0000

    Add create page form objects

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,21 @@
+ //navigation function 
   $(document).ready(function(){
     $('.collapsible').collapsible();
   });
+
+  //create sprint form function
+    $(document).ready(function() {
+    M.updateTextFields();
+  });
+  
+  //jQuery method for select box, not longer a dependency, applied custom javascript
+    document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
+
+   var instance = M.FormSelect.getInstance(elem);
+
+   //slider jquery
+   $( "#slider" ).slider();
+ 
