386243d34244150e0e337bc6820c878cd2bcc022
michodgs24 <michodgs@gmail.com>
2020-08-28 12:14:44 +0000

    Update create page form& add jQuery functionality

--- static/js/script.js
+++ static/js/script.js
@@ -8,7 +8,7 @@
     M.updateTextFields();
   });
   
-  //jQuery method for select box, not longer a dependency, applied custom javascript
+  //jQuery method for select boxes, not longer a dependency, applied custom javascript
     document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, options);
@@ -16,6 +16,29 @@
 
    var instance = M.FormSelect.getInstance(elem);
 
-   //slider jquery
-   $( "#slider" ).slider();
- 
+   // trigger an autoresize for textarea 1& 2
+    $('#textarea1', '#textarea2').val('New Text');
+  M.textareaAutoResize($('#textarea1', '#textarea2'));
+
+   //call slider
+   $( "slider" ).slider();
+   
+   //Initialize the slider with the slide callback 
+   $( ".selector" ).slider({
+  start: function( event, ui ) {}
+});
+//Bind an event listener to the slidestart event
+$( ".selector" ).on( "slidestart", function( event, ui ) {} );
+
+//Initialize the slider with the stop callback
+$( ".selector" ).slider({
+  stop: function( event, ui ) {}
+});
+
+//Bind an event listener to the slidestop event
+$( ".selector" ).on( "slidestop", function( event, ui ) {} );
+
+//call date-picker
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
