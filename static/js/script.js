f69af67e70a6ebe30122b6926505884d7c592612
michodgs24 <michodgs@gmail.com>
2020-08-28 22:29:22 +0000

    Update jQuery function call for create page input form

--- static/js/script.js
+++ static/js/script.js
@@ -1,44 +1,47 @@
- //navigation function 
+ //call document function
   $(document).ready(function(){
+    //collapsible navigation
     $('.collapsible').collapsible();
-  });
 
-  //create sprint form function
-    $(document).ready(function() {
+    // trigger an autoresize for textarea 1& 2
+    $('#textarea1', '#textarea2').val('New Text');
+    M.textareaAutoResize($('#textarea1', '#textarea2'));
+    
+    //call date-picker
+    $('.datepicker').datepicker();
+    
+    // open date picker
+    instance.open();
+    
+    //close date picker
+    instance.close();
+    
+    //get date instance
+    instance.toString();
+    
+    //destroy date
+    instance.destroy();
+    
+    //get instance of string date
+    instance.toString();
+    
+    //set new date
+    instance.setDate(new Date());
+    
+    //Date to show on the datepicker
+    instance.gotoDate(new Date());
+    
+    //create sprint text form function
     M.updateTextFields();
+    
+    //call slider
+    $( "slider" ).slider();
   });
   
-  //jQuery method for select boxes, not longer a dependency, applied custom javascript
+  //jQuery method for select boxes no longer a materialize library dependency, applied custom javascript
     document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, options);
   });
 
    var instance = M.FormSelect.getInstance(elem);
-
-   // trigger an autoresize for textarea 1& 2
-    $('#textarea1', '#textarea2').val('New Text');
-  M.textareaAutoResize($('#textarea1', '#textarea2'));
-
-   //call slider
-   $( "slider" ).slider();
-   
-   //Initialize the slider with the slide callback 
-   $( ".selector" ).slider({
-  start: function( event, ui ) {}
-});
-//Bind an event listener to the slidestart event
-$( ".selector" ).on( "slidestart", function( event, ui ) {} );
-
-//Initialize the slider with the stop callback
-$( ".selector" ).slider({
-  stop: function( event, ui ) {}
-});
-
-//Bind an event listener to the slidestop event
-$( ".selector" ).on( "slidestop", function( event, ui ) {} );
-
-//call date-picker
-  $(document).ready(function(){
-    $('.datepicker').datepicker();
-  });
