e0b94a0c9a0c94d15c25109101a03e2689df48ee
michodgs24 <michodgs@gmail.com>
2020-09-04 12:11:22 +0000

    Update task call queries formatting

--- app.py
+++ app.py
@@ -42,20 +42,20 @@ def get_create():
 @app.route("/add_activity", methods=["GET", "POST"])
 def add_activity():
     task = {
-        "name": request.form.get("name"),
-        "surname": request.form.get("surname"),
-        "gender": request.form.getlist("gender"),
-        "age": request.form.get("age"),
-        "activity": request.form.get("activity"),
-        "title": request.form.get("title"),
-        "description": request.form.get("description"),
-        "difficulty": request.form.getlist("difficulty"),
-        "date": request.form.get("date")
+        "task_name": request.form.get("task_name"),
+        "task_surname": request.form.get("task_surname"),
+        "task_gender": request.form.getlist("task_gender"),
+        "task_age": request.form.get("task_age"),
+        "task_activity": request.form.get("task_activity"),
+        "task_title": request.form.get("task_title"),
+        "task_description": request.form.get("task_description"),
+        "task_difficulty": request.form.getlist("task_difficulty"),
+        "task_date": request.form.get("task_date")
     }
     if request.method == "POST":
         mongo.db.tasks.insert_one(task)
         flash("Activity Successfully added")
-        return redirect(url_for("get_create_compare"))
+        return redirect(url_for("get_tasks"))
 
     return render_template("add_activity.html")
 
