85982545108aad1c2cf5ecdc938eccfd8ec2f4c9
michodgs24 <michodgs@gmail.com>
2020-09-04 13:33:28 +0000

    Update task for loop and request form method

--- app.py
+++ app.py
@@ -41,21 +41,21 @@ def get_create():
 
 @app.route("/add_activity", methods=["GET", "POST"])
 def add_activity():
-    task = {
-        "task_name": request.form.get("task_name"),
-        "task_surname": request.form.get("task_surname"),
-        "task_gender": request.form.getlist("task_gender"),
-        "task_age": request.form.get("task_age"),
-        "task_activity": request.form.get("task_activity"),
-        "task_title": request.form.get("task_title"),
-        "task_description": request.form.get("task_description"),
-        "task_difficulty": request.form.getlist("task_difficulty"),
-        "task_date": request.form.get("task_date")
-    }
     if request.method == "POST":
-        mongo.db.tasks.insert_one(task)
-        flash("Activity Successfully added")
-        return redirect(url_for("get_tasks"))
+        task = {
+            "task_name": request.form.get("task_name"),
+            "task_surname": request.form.get("task_surname"),
+            "task_gender": request.form.getlist("task_gender"),
+            "task_age": request.form.get("task_age"),
+            "task_activity": request.form.getlist("task_activity"),
+            "task_title": request.form.get("task_title"),
+            "task_description": request.form.getlist("task_description"),
+            "task_difficulty": request.form.getlist("task_difficulty"),
+            "task_date": request.form.get("task_date")
+            }
+    mongo.db.tasks.insert_one(task)
+    flash("Activity Successfully added")
+    return redirect(url_for("get_tasks"))
 
     return render_template("add_activity.html")
 
