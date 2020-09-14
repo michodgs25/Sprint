bd471cac3d08f80075f64e9bc467a14e950df561
michodgs24 <michodgs@gmail.com>
2020-09-14 22:18:15 +0000

    Update navigation bar

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,24 @@
- //call document function
-  $(document).ready(function(){
-    
+$(document).ready(function(){
     //Trigger accordian collapse
     $('.collapsible').collapsible();
-   
-    // trigger an autoresize for textarea 1& 2
-if ($('#textarea1').length > 0) {
-    $('#textarea1').val('New Text');
-    M.textareaAutoResize($('#textarea1'));
-}
-
-if ($('#textarea2').length > 0) {
-    $('#textarea2').val('New Text');
-    M.textareaAutoResize($('#textarea2'));
-}
+    
+    //trigger an autoresize for textarea 1& 2
+    if ($('#textarea1').length > 0) {
+        $('#textarea1').val('New Text');
+        M.textareaAutoResize($('#textarea1'));
+    }
+    
+    if ($('#textarea2').length > 0) {
+        $('#textarea2').val('New Text');
+        M.textareaAutoResize($('#textarea2'));
+    }
+    
     //create sprint text form function
     M.updateTextFields();
-
+            
     //Trigger select box options
     $('select').formSelect();
-    
+            
     //call datepicker
-      $(document).ready(function(){
     $('.datepicker').datepicker();
 });
