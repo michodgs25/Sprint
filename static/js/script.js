c26e378b9e3ad3438e17c7a2fd7e35ce236d79f0
michodgs24 <michodgs@gmail.com>
2020-09-15 16:37:23 +0000

    Add save and edit button to explore page logs

--- static/js/script.js
+++ static/js/script.js
@@ -20,5 +20,12 @@ $(document).ready(function(){
     $('select').formSelect();
             
     //call datepicker
-    $('.datepicker').datepicker();
+    $('.datepicker').datepicker({
+        format: "dd, mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
