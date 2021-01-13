82a5a8e514a5d0526445adf24a6ef23d9cb784d9
michodgs25 <michodgs@gmail.com>
2021-01-13 12:46:40 +0000

    Add register page

--- app.py
+++ app.py
@@ -3,14 +3,19 @@ from flask import (
     Flask, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import (
+     generate_password_hash, check_password_hash)
+
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
 mongo = PyMongo(app)
 
 
@@ -24,6 +29,11 @@ def get_index():
     the index.html document.
     """
     return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 @app.route("/sprints")
